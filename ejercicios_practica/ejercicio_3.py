4fe5b64361669787b5603a42497d2f2ccfddb77e
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-07-26 21:36:36 -0300

    Update ejercicio_3.py

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -23,6 +23,19 @@ numero_2 = -2
 #       --> En caso afirmativo imprima en pantalla "Resp=3"
 #       --> En caso negativo imprima en pantalla "Resp=4"
 
+
+if (numero_1 > 5):
+    if (numero_2 > 0):
+        print("Resp=1")
+    else:
+        print("Resp=2")
+elif (numero_1 < 5):
+    if numero_2 > 5:
+     print("Resp=3") 
+    else:
+        print("Resp=4") 
+        
+        
 # Verifique la calificación de un estudiante según su
 # puntaje en un examen
 puntaje = 70
