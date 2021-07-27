dd4ab09efef6e8b4a5e30fc156f0a43c35059a42
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-07-27 01:07:50 -0300

    Update profundizacion_3.py

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -24,5 +24,33 @@ De las temperaturas ingresadas por consola determinar:
 En cada caso imprimir en pantalla el resultado
 '''
 
+temp1 = float(input("Ingrese la primer temperatura\n"))
+temp2 = float(input("Ingrese la segunda temperatura\n"))
+temp3 = float(input("Ingrese la tercera temperatura\n"))
+
+if (temp1 > temp2) and (temp1 < temp3):
+        print("La temperatura maxima ingresada es {}".format(temp1))
+elif (temp2 > temp1) and (temp2 > temp3):
+    print("La temperatura maxima ingresada es {}".format(temp2))
+elif (temp3 > temp1) and (temp3 > temp2):
+    print("La temperatura maxima ingresada es {}".format(temp3))
+    
+print("\n")
+
+if (temp1 < temp2) and (temp1 < temp3):
+        print("La temperatura minima ingresada es {}".format(temp1))
+elif (temp2 < temp1) and (temp2 < temp3):
+    print("La temperatura minima ingresada es {}".format(temp2))
+elif (temp3 < temp1) and (temp3 < temp2):
+    print("La temperatura minima ingresada es {}".format(temp3))
+            
+print("\n")        
+prom= (temp1+temp2+temp3)/3
+print("El promedio de las temperaturas ingresadas es:{}".format(prom))
+
+
+print("\n")
+
+
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
