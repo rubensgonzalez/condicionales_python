05456cf2d338409b159af052557c7dc3cba2e45c
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-07-26 21:41:32 -0300

    Update ejercicio_3.py

--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -48,3 +48,14 @@ puntaje = 70
 
 # Debe imprimir en pantalla la calificacion
 # Utilizar "if" anidados
+
+if puntaje >= 90:
+    print("A")
+elif puntaje >= 80:
+    print("B")
+elif puntaje >= 70:
+    print("C")
+elif puntaje >= 60:
+    print("D")
+elif puntaje < 60:
+    print("F")
