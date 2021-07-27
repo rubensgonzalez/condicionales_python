8f4f776d10f280d5faf0aefd7b3a355e05030a9e
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-07-27 00:41:13 -0300

    Update profundizacion_2.py

--- ejercicios_profunidzacion/profundizacion_2.py
+++ ejercicios_profunidzacion/profundizacion_2.py
@@ -21,5 +21,26 @@ o impar.
 Para cada caso imprimir el resultado en pantalla.
 '''
 
+numero1 = int(input("Ingrese el primer numero\n"))
+numero2 = int(input("Ingrese el segundo numero\n"))
+numero3 = int(input("Ingres el tercer numero\n"))
+
+if (numero1 % 2) == 0:
+    print("El primer numero {} es: Par ".format(numero1))
+else:
+    print("El primer numero {} es: Impar".format(numero1))
+
+if (numero2 % 2) == 0:
+    print("El primer numero {} es: Par ".format(numero2))
+else:
+    print("El primer numero {} es: Impar".format(numero2))
+
+if (numero3 % 2) == 0:
+    print("El primer numero {} es: Par ".format(numero3))
+else:
+    print("El primer numero {} es: Impar".format(numero3))
+
+
+
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
