c52153b3c3eb564f903f3f54a41f4ceaad43d9f0
Ruben <87348517+rubensgonzalez@users.noreply.github.com>
2021-07-27 00:06:09 -0300

    Update ejercicio_4.py

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -15,10 +15,27 @@ texto_2 = '7'
 # Verifique cual cual de los dos textos es mayor alfabéticamente
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2:
+    print("El mayor textos es texto_1: {}".format(texto_1))
+
+else:
+    print("El mayor es texto_2: {}".format(texto_2))
+
 # Transforma esas variables tipo texto y almacénalas
 # en nuevas variables númericas (int)
 # Repita el proceso, ¿Cuál de las nuevas variables es mayor?
 # Imprima en pantalla según corresponda
+
+texto_1 = int(texto_1)
+
+texto_2=int(texto_2)
+
+if texto_1 > texto_2:
+    print("El mayor textos es texto_1: {}".format(texto_1))
+
+else:
+    print("El mayor es texto_2: {}".format(texto_2))
+
 
 # Para pensar!
 # ¿Por qué cree que texto_2 es mayor a texto_1?
@@ -28,3 +45,6 @@ texto_2 = '7'
 # Esta pregunta estará repetida en el Campus para que puedan
 # responder.
 # NOTA: La respuesta no se encuentra en el apunte, sino en Google ;)
+
+#La respuesta es que en la tabla ASCII tienen el mismo valor
+# por eso siempre texto_2 es mayor que texto_1 en los dos casos
